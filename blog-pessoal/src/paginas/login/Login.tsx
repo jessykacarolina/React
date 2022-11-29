633f8dbbb9f6cc737c3d187f2e7fb17e69d461a2
jessykacarolina <jessyka_carolina_2010@hotmail.com>
2022-11-29 11:08:12 -0300

    first commit

--- blog-pessoal/src/paginas/login/Login.tsx
+++ blog-pessoal/src/paginas/login/Login.tsx
@@ -2,6 +2,7 @@ import { Button, Grid, TextField, Typography } from '@material-ui/core';
 import { Box } from '@mui/material';
 import React from 'react';
 import { Link } from 'react-router-dom'
+import './Login.css'
 
 function Login() {
     return (
@@ -24,15 +25,11 @@ function Login() {
                     <Box marginRight={1}>
                         <Typography variant='subtitle1' gutterBottom align='center'>Não tem uma conta?</Typography>
                     </Box>
-                    <Typography variant='subtitle1' gutterBottom align='center' style={{fontWeight: 'bold'}}>Cadastre-se</Typography>
+                    <Typography variant='subtitle1' gutterBottom align='center' className='textos1'>Cadastre-se</Typography>
                 </Box>
             </Box>
          </Grid>
-         <Grid xs={6} style={{
-            backgroundImage: `url(https://i.imgur.com/d5bMdDJ.jpg)`,
-            backgroundRepeat: 'no-repeat', width: '100vh', minHeight: '100vh', backgroundSize: 'cover', backgroundPosition: 'center'
-         }}>
-
+         <Grid xs={6} className='imagem'>
          </Grid>
         </Grid>
     );
